a24378cfd0791d164e1b46a7ab7727d068d75f45
Xai <87550624+kaxem@users.noreply.github.com>
2023-01-22 05:31:08 +0330

    script added

---
+++ ir-mkr.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# bash script made by The God himself to creat an iranian
+
+MAX=$(($(($RANDOM%3))+98))
+touch irani_wishes
+irani_misfortune=0 ##At birth time
+irani_salary='none'
+rm irani_wishes
+irani_salary=$MIN
+irani_misfortune=$MAX
+echo 'Congrates You made an Iranian human!\n'
